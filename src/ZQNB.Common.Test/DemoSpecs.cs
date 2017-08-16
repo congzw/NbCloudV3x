53e8cac47fdd7e59df2af49a4c4c255879e1586c
congzw <46074987@qq.com>
2017-08-16 13:54:11 +0800

    add Common

---
+++ src/ZQNB.Common.Test/DemoSpecs.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZQNB.Common.Test
+{
+    [TestClass]
+    public class DemoSpecs
+    {
+        [TestMethod]
+        public void Demo_Run_Should_Ok()
+        {
+        }
+    }
+}
