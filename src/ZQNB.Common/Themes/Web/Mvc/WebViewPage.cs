9d8357ae7cb7e1b43ca3718350202ff985faa389
congzw <46074987@qq.com>
2017-08-17 14:35:43 +0800

    add theme support

---
+++ src/ZQNB.Common/Themes/Web/Mvc/WebViewPage.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+
+namespace ZQNB.Common.Themes.Web.Mvc
+{
+    public abstract class WebViewPage<TModel> : System.Web.Mvc.WebViewPage<TModel>
+    {
+        public override string Layout
+        {
+            get
+            {
+                var layout = base.Layout;
+
+                if (!string.IsNullOrEmpty(layout))
+                {
+                    var filename = System.IO.Path.GetFileNameWithoutExtension(layout);
+                    ViewEngineResult viewResult = ViewEngines.Engines.FindPartialView(ViewContext.Controller.ControllerContext, filename);
+                    //ViewEngineResult viewResult = ViewEngines.Engines.FindView(ViewContext.Controller.ControllerContext, filename, "");
+
+                    if (viewResult.View != null && viewResult.View is RazorView)
+                    {
+                        layout = ((RazorView) viewResult.View).ViewPath;
+                    }
+                }
+
+                return layout;
+            }
+            set
+            {
+                base.Layout = value;
+            }
+        }
+    }
+
+    public abstract class WebViewPage : WebViewPage<dynamic>
+    {
+    }
+}
