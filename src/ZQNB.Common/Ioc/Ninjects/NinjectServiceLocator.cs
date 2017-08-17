9fab8aee5fba8e9407d0d9be7a7853bdbc88ff92
congzw <46074987@qq.com>
2017-08-17 12:41:50 +0800

    remove common.impl

---
+++ src/ZQNB.Common/Ioc/Ninjects/NinjectServiceLocator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Ninject;
+
+namespace ZQNB.Common.Ioc.Ninjects
+{
+    /// <summary>
+    /// NinjectServiceLocator
+    /// </summary>
+    public class NinjectServiceLocator : ServiceLocatorImplBase
+    {
+        public Func<IKernel> KernelFunc { get; private set; }
+
+        /// <summary>
+        /// CTOR
+        /// </summary>
+        /// <param name="kernelFunc"></param>
+        public NinjectServiceLocator(Func<IKernel> kernelFunc)
+        {
+            KernelFunc = kernelFunc;
+        }
+
+        protected override object DoGetInstance(Type serviceType, string key)
+        {
+            var kernel = KernelFunc.Invoke();
+            if (key == null)
+            {
+                return kernel.TryGet(serviceType);
+            }
+            return kernel.TryGet(serviceType, key);
+        }
+
+        protected override IEnumerable<object> DoGetAllInstances(Type serviceType)
+        {
+            var kernel = KernelFunc.Invoke();
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
