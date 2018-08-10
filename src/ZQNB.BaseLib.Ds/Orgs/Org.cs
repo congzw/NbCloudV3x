90b79db18b306c8ca321443a555477cc54700b12
congzw <2981810300@qq.com>
2018-08-10 11:56:22 +0800

    add Ds projects

---
+++ src/ZQNB.BaseLib.Ds/Orgs/Org.cs
@@ -0,0 +1,16 @@
+﻿using ZQNB.Common.Data.Model;
+
+namespace ZQNB.BaseLib.Ds.Orgs
+{
+    public class Org : NbEntity<Org>
+    {
+        public virtual string Name { get; set; }
+        public virtual string OrgTypeCode { get; set; }
+    }
+
+    public class OrgType : NbEntity<OrgType>
+    {
+        public virtual string Code { get; set; }
+        public virtual string Name { get; set; }
+    }
+}
